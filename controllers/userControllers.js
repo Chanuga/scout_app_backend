dedb8912d653f6520ca4d9f3afdaa17489dccc4c
chanuga.jayathilaka <chanuga.jayathilaka@ecologital.com>
2023-11-05 20:13:22 +0530

    reference errer fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-import { UserModel } from '../models/UserModel.js'
+import { UserModel } from '../models/userModel.js'
 import bcrypt from 'bcrypt';
 import validator from 'validator';
 import jwt from 'jsonwebtoken'
